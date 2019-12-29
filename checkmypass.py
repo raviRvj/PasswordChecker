5a6530c86fdebb1ad355dad6681822897ae29344
raviRvj <raviraj.it98@gmail.com>
2019-12-30 01:00:44 +0530

    Uploading the source code

---
+++ checkmypass.py
@@ -0,0 +1,45 @@
+import requests
+import hashlib
+import sys
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    # print(hashes.text)
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        # print(h, count)
+        if h == hash_to_check:
+            return count
+    return 0
+
+
+def pwned_api_check(password):
+    # Check password if it exists in API response
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first_five_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first_five_char)
+    # print(first_five_char, tail)
+    # print(response)
+    return get_password_leaks_count(response, tail)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times. Secure your password')
+        else:
+            print(f'{password} was not found.')
+    return 'done!'
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
